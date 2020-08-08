2c49d2e02d0a59c90a52c42bfb4b113123993b18
ysaleem <yusesale@gmail.com>
2020-08-08 15:55:43 -0700

    code for processArgs

--- myls.c
+++ myls.c
@@ -6,39 +6,34 @@
 #include <stdlib.h>
 
 // Citation: https://stackoverflow.com/questions/3554120/open-directory-using-c
-bool flags[3]; // -i, -l, -R
+bool flags[3] = {false}; // -i, -l, -R
 
 int main (int argc, char *argv[]) {
 
-	flags[] = {false, false, false}; // -i, -l, -R
-	
+	processArgs(argc, argv);
+
 }
 
 void processArgs(int argc, char *argv[]) {
-	
+	for (int i = 0; i < argc; i++) {
+		// Check if argv[i] is an option
+		if (argv[i][0] == '-') {
+			parseOption(argv[i]);
+		} else {
+			// argv[i] is a directory/file
+			printFiles(argv[i]);
+		}
+
+	}
 }
 
 void parseOption(char* option) {
-	
+	printf("Received option: %s\n",option)
 }
 
 void printFiles(char* dir) {
-	struct dirent *pDirent;
-	DIR *pDir;
-	
-	// Ensure we can open directory.
-    pDir = opendir (fileList[i]);
-    if (pDir == NULL) {
-        printf ("Cannot open directory '%s'\n", argv[1]);
-            return 1;
-        }
-	}
-	
-	// print file index (iNode number)
-	printf("%ld ", pDirent->d_ino);
-	
-	// Print file name
-	printf ("%s", pDirent->d_name);
-	
-	closedir (pDir);
+
+	printf("Received dir: %s\n", dir);
+
+
 }
