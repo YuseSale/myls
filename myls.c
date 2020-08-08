94addd49943681f097a326f5f6bf6aa9b1165ef4
John Wu <john_wu966@hotmail.com>
2020-08-08 15:52:19 -0700

    Added header files. Changed parseOption

--- myls.c
+++ myls.c
@@ -4,41 +4,49 @@
 #include <sys/stat.h>
 #include <stdbool.h>
 #include <stdlib.h>
-
+#include "myls.h"
 // Citation: https://stackoverflow.com/questions/3554120/open-directory-using-c
 bool flags[3]; // -i, -l, -R
 
-int main (int argc, char *argv[]) {
 
-	flags[] = {false, false, false}; // -i, -l, -R
-	
+
+int main (int argc, char *argv[]) {
+	flags[0],flags[1],flags[2] = false; // -i, -l, -R
+	char* test = "kappa";
+	printf("TestText2\n");
+	parseOption(test);
 }
+
+
 
 void processArgs(int argc, char *argv[]) {
 	
 }
 
 void parseOption(char* option) {
-	
+	printf("TestText\n");
+	for (int i = 0; i <sizeof(option); i++){
+		printf("%s\n", option[i]);
+	}
 }
 
 void printFiles(char* dir) {
-	struct dirent *pDirent;
-	DIR *pDir;
+	// struct dirent *pDirent;
+	// DIR *pDir;
 	
-	// Ensure we can open directory.
-    pDir = opendir (fileList[i]);
-    if (pDir == NULL) {
-        printf ("Cannot open directory '%s'\n", argv[1]);
-            return 1;
-        }
-	}
+	// // Ensure we can open directory.
+    // pDir = opendir (fileList[i]);
+    // if (pDir == NULL) {
+    //     printf ("Cannot open directory '%s'\n", argv[1]);
+    //         return 1;
+    //     }
+	// }
 	
-	// print file index (iNode number)
-	printf("%ld ", pDirent->d_ino);
+	// // print file index (iNode number)
+	// printf("%ld ", pDirent->d_ino);
 	
-	// Print file name
-	printf ("%s", pDirent->d_name);
+	// // Print file name
+	// printf ("%s", pDirent->d_name);
 	
-	closedir (pDir);
+	// closedir (pDir);
 }
