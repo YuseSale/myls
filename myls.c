d431e913fe93c045da6ab7b112c8ae82ad9c5608
John Wu <john_wu966@hotmail.com>
2020-08-08 22:36:31 -0700

    worked on LexiSort

--- myls.c
+++ myls.c
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include "myls.h"
+#include <ctype.h>
 // Citation: https://stackoverflow.com/questions/3554120/open-directory-using-c
 bool flags[3] = {false}; // -i, -l, -R
 
@@ -13,18 +14,33 @@ bool flags[3] = {false}; // -i, -l, -R
 int main (int argc, char *argv[]) {
 	flags[0],flags[1],flags[2] = false; // -i, -l, -R
 
-
-	// testParseOption();
-	// testFlags();
-	
-	processArgs(argc, argv);
+	// testIsLower();
+	testLexiSort(); 
+	// // testParseOption();
+	// // testFlags();
+	// char* testArray[] = {"apple","banana","candy","Text","colin","table"};
+	// // processArgs(argc, argv);
 
 
 	return 0;
 }
+void testIsLower(){
+	char test1[MAX_WORD_LENGTH] = "";
+	printf("Enter a word: ");
 
+    scanf("%s", test1);  
+	char test2[MAX_WORD_LENGTH] = "";
+	printf("Enter a word: ");
+
+    scanf("%s", test2);
+	if (isLower(test1,test2)) {
+		printf("[%s < %s\n]",test1,test2);
+	} else{
+		printf("[%s < %s]\n",test2,test1);
+	}
+}
 void testParseOption(){
-	char test[20] = "";
+	char test[MAX_WORD_LENGTH] = "";
 	printf("Enter a word: ");
 
     scanf("%s", test);  
@@ -37,22 +53,22 @@ void testFlags(){
 	printf("-R flag: %d\n",flags[2]);
 }
 
-void processArgs(int argc, char *argv[]) {
+// void processArgs(int argc, char *argv[]) {
 
-	// Just ./myls
-	if (argc == 1) {
-		printFiles(".");
-		return;
-	}
+// 	// Just ./myls
+// 	if (argc == 1) {
+// 		printFiles(".");
+// 		return;
+// 	}
 
-	for (int i = 1; i < argc; i++) {
-		if (argv[i][0] == '-') {
-			parseOption(argv[i]);
-		} else {
-			printFiles(argv[i]);
-		}
-	}
-}
+// 	for (int i = 1; i < argc; i++) {
+// 		if (argv[i][0] == '-') {
+// 			parseOption(argv[i]);
+// 		} else {
+// 			printFiles(argv[i]);
+// 		}
+// 	}
+// }
 
 void parseOption(char* option) {
 
@@ -106,14 +122,74 @@ void parseOption(char* option) {
 // }
 
 void readDirectory (char* dir) {
-
-	stuct dirent entityQueue[10];
+	struct dirent* entityQueue[10];
 	struct dirent* pDirent;
 	DIR *pDir;
+}
+void testLexiSort(){
+	char table[8][MAX_WORD_LENGTH] = {"word", "that", "is","safe","for","kids","to","read"};
 
 
+	for (int i = 0; i <8; i++){
+		printf("%i: %s\n", i, table[i]);
+	}
+	printf("Sorting....\n");
+	LexiSort(table,8);
 
 }
+
+
+void LexiSort(char table[][MAX_WORD_LENGTH],int numOfEntries){	
+
+	char lowest[40] = "";
+	int lowestIndex;
+	for (int i = 0; i<numOfEntries;i++){
+		strcpy(lowest,table[i]);
+
+
+		lowestIndex=i;
+
+		for (int j = i; j<numOfEntries; j++){
+			if (isLower(table[j],lowest)){
+				
+				strcpy(lowest, table[j]);
+				lowestIndex=j;
+			}
+		}
+
+
+		strcpy(table[lowestIndex],table[i]);
+		
+		strcpy(table[i], lowest);
+		
+	}
+
+	for (int i = 0; i <numOfEntries; i++){
+		printf("%i: %s\n", i, table[i]);
+	}
+}
+//tests if 'first' is lexicographically smaller than 'second'
+bool isLower(char* first, char* second){
+	for (int i = 0; i < strlen(first) || i <strlen(second); i++){
+		if (tolower(first[i])<tolower(second[i])){
+			return true;
+		}
+		else if (tolower(first[i])>tolower(second[i])){
+			return false;
+		}
+		else if (i = strlen(second)){
+			return false;
+		}
+		else if (i = strlen(first)){
+			return true;
+		}
+		
+	}
+
+
+	
+}			
+
 
 void sortEntityQueue(struct dirent* entityQueue) {
 
