b9b8c967419b8b0e98fec9779546036d902f9a1d
John Wu <john_wu966@hotmail.com>
2020-08-08 23:53:00 -0700

    Successfully implemented sortEntityQueue

--- myls.c
+++ myls.c
@@ -12,15 +12,14 @@ bool flags[3] = {false}; // -i, -l, -R
 
 
 int main (int argc, char *argv[]) {
-	flags[0],flags[1],flags[2] = false; // -i, -l, -R
+
 
 	// testIsLower();
-	testLexiSort(); 
+	// testLexiSort(); 
 	// // testParseOption();
 	// // testFlags();
-	// char* testArray[] = {"apple","banana","candy","Text","colin","table"};
-	// // processArgs(argc, argv);
 
+	processArgs(argc, argv);
 
 	return 0;
 }
@@ -53,22 +52,23 @@ void testFlags(){
 	printf("-R flag: %d\n",flags[2]);
 }
 
-// void processArgs(int argc, char *argv[]) {
+void processArgs(int argc, char *argv[]) {
 
-// 	// Just ./myls
-// 	if (argc == 1) {
-// 		printFiles(".");
-// 		return;
-// 	}
+	// Just ./myls	
+	if (argc == 1) {	
+		readDirectory(".");	
+		return;	
+	}	
 
-// 	for (int i = 1; i < argc; i++) {
-// 		if (argv[i][0] == '-') {
-// 			parseOption(argv[i]);
-// 		} else {
-// 			printFiles(argv[i]);
-// 		}
-// 	}
-// }
+
+	for (int i = 1; i < argc; i++) {	
+		if (argv[i][0] == '-') {	
+			parseOption(argv[i]);
+		} else {
+			readDirectory(argv[i]);
+		}
+	}
+}
 
 void parseOption(char* option) {
 
@@ -84,48 +84,101 @@ void parseOption(char* option) {
 				flags[1] = true;
 				printf("DEBUG :Changed Flag -l to true. \n");
 				break;
-			} 
+			}
 			case 'R':{
 				flags[2] = true;
 				printf("DEBUG: Changed Flag -R to true. \n");
 				break;
-			} 
+			}
 		}
-		
 	}
 }
 
-// void printFiles(char* dir) {
-// 	// Given a directory path, print all files & folders in the directory
-
-// 	struct dirent *pDirent;
-// 	DIR *pDir;
-
-// 	// Ensure we can open directory.
-// 	// If can't, it's a file, so just print it
-// 	pDir = opendir(dir);
-// 	if (pDir == NULL) {
-// 		printf("%s", dir);
-// 		return;
-// 	}
-
-// 	// Print all files in the directory
-// 	// If subdirectories found, recursively print em
-// 	while((pDirent=readdir(pDir))!=NULL) {
-// 		printf("%s  ", pDirent->d_name);
-// 	}
-// 	printf("\n");
-// 	// print file index (iNode number)
-// 	//printf("%ld ", pDirent->d_ino);
-
-// 	closedir (pDir);
-// }
 
 void readDirectory (char* dir) {
-	struct dirent* entityQueue[10];
+
+
+	int entityQueueMax = 10;
+	//struct dirent* entityQueue[entityQueueMax];
+	struct dirent** entityQueue = malloc(entityQueueMax*8);
+	int entityQueueCount = 0;
 	struct dirent* pDirent;
 	DIR *pDir;
+
+	char* dirQueue[entityQueueMax];
+	int dirQueueCount = 0;
+
+	// Check if dir itself is an accessable directory
+	pDir = opendir(dir);
+	if (pDir == NULL) {
+		printf("%s", dir);
+		return;
+	}
+
+	// Read all the entities in dir
+	while ((pDirent = readdir(pDir)) != NULL) {
+
+		// Check if entityQueue is full
+		if (entityQueueCount == entityQueueMax) {
+			//TODO: remalloc bigger array
+			entityQueueMax = entityQueueMax * 2;
+			entityQueue = realloc(entityQueue, entityQueueMax*8);
+		}
+
+		
+		if (pDirent->d_name[0] != '.') {
+			entityQueue[entityQueueCount] = pDirent;
+			entityQueueCount++;
+		}
+		
+	}
+	closedir(pDir); 
+
+	sortEntityQueue(entityQueue,entityQueueCount);
+
+	for (int i = 0; i < entityQueueCount; i++) {
+
+		if (entityQueue[i]->d_name[0] == '.') {
+			continue;
+		}
+
+		// print the entity
+		printf("%s  ", entityQueue[i]->d_name);
+		// Create a directory string for it
+		char* goDir = malloc(255);
+
+		strcpy(goDir, dir);
+		strcat(goDir, "/");
+		strcat(goDir, entityQueue[i]->d_name);
+
+		// Check if the entity was an accessible directory
+		pDir = opendir(goDir);
+
+		if (pDir != NULL) {
+			// This is an accessable directory, enqueue it
+			dirQueue[dirQueueCount] = goDir;
+			dirQueueCount++;
+		}
+		closedir(pDir);
+
+	}
+
+	printf("\n\n");
+
+	for (int i = 0; i < dirQueueCount; i++) {
+		printf("%s: \n", dirQueue[i]);
+		readDirectory(dirQueue[i]);
+	}
+
+	// Free all malloced memory
+	free(entityQueue);
+	for (int i = 0; i < dirQueueCount; i++) {
+		free(dirQueue[i]);
+	}
+
+	return;
 }
+
 
 
 void testLexiSort(){
@@ -143,7 +196,7 @@ void testLexiSort(){
 
 void LexiSort(char table[][MAX_WORD_LENGTH],int numOfEntries){	
 
-	char lowest[40] = "";
+	char lowest[MAX_WORD_LENGTH] = "";
 	int lowestIndex;
 	for (int i = 0; i<numOfEntries;i++){
 		strcpy(lowest,table[i]);
@@ -166,9 +219,7 @@ void LexiSort(char table[][MAX_WORD_LENGTH],int numOfEntries){
 		
 	}
 
-	for (int i = 0; i <numOfEntries; i++){
-		printf("%i: %s\n", i, table[i]);
-	}
+
 }
 //tests if 'first' is lexicographically smaller than 'second'
 bool isLower(char* first, char* second){
@@ -179,10 +230,12 @@ bool isLower(char* first, char* second){
 		else if (tolower(first[i])>tolower(second[i])){
 			return false;
 		}
-		else if (i = strlen(second)){
+
+
+		if (i == strlen(second)){
 			return false;
 		}
-		else if (i = strlen(first)){
+		else if (i == strlen(first)){
 			return true;
 		}
 		
@@ -193,6 +246,33 @@ bool isLower(char* first, char* second){
 }			
 
 
-void sortEntityQueue(struct dirent* entityQueue) {
+void sortEntityQueue(struct dirent** entityQueue,int numOfEntries) {
+	char lowest[MAX_WORD_LENGTH] = "";
+	int lowestIndex;
+	for (int i = 0; i<numOfEntries;i++){
+		strcpy(lowest,entityQueue[i]->d_name);
+		lowestIndex=i;
+
+		for (int j = i; j<numOfEntries; j++){
+			if (isLower(entityQueue[j]->d_name ,lowest)){
+				
+				strcpy(lowest, entityQueue[j]->d_name );
+				lowestIndex=j;
+			}
+		}
+
+		struct dirent* lowestEntity = entityQueue[lowestIndex];
+
+		
+		entityQueue[lowestIndex] = entityQueue[i];
+		entityQueue[i] = lowestEntity;
+
+
+		// strcpy(table[lowestIndex],table[i]);
+		
+		// strcpy(table[i], lowest);
+		
+	}
+
 
 }
