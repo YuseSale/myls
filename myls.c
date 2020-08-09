ee994d426d71c16211c3df5c61bcb7325374b1c9
ysaleem <yusesale@gmail.com>
2020-08-08 20:36:15 -0700

    Recursive functionality (default)

--- myls.c
+++ myls.c
@@ -11,14 +11,8 @@ bool flags[3] = {false}; // -i, -l, -R
 
 
 int main (int argc, char *argv[]) {
-	flags[0],flags[1],flags[2] = false; // -i, -l, -R
 
-
-	// testParseOption();
-	// testFlags();
-	
 	processArgs(argc, argv);
-
 
 	return 0;
 }
@@ -27,10 +21,11 @@ void testParseOption(){
 	char test[20] = "";
 	printf("Enter a word: ");
 
-    scanf("%s", test);  
+    scanf("%s", test);
 	parseOption(&test[0]);
-	
+
 }
+
 void testFlags(){
 	printf("-i flag: %d\n",flags[0]);
 	printf("-l flag: %d\n",flags[1]);
@@ -41,7 +36,7 @@ void processArgs(int argc, char *argv[]) {
 
 	// Just ./myls
 	if (argc == 1) {
-		printFiles(".");
+		readDirectory(".");
 		return;
 	}
 
@@ -49,7 +44,7 @@ void processArgs(int argc, char *argv[]) {
 		if (argv[i][0] == '-') {
 			parseOption(argv[i]);
 		} else {
-			printFiles(argv[i]);
+			readDirectory(argv[i]);
 		}
 	}
 }
@@ -63,19 +58,19 @@ void parseOption(char* option) {
 				flags[0] = true;
 				printf("DEBUG: Changed Flag -i to true. \n");
 				break;
-			} 
+			}
 			case 'l':{
 				flags[1] = true;
 				printf("DEBUG :Changed Flag -l to true. \n");
 				break;
-			} 
+			}
 			case 'R':{
 				flags[2] = true;
 				printf("DEBUG: Changed Flag -R to true. \n");
 				break;
-			} 
+			}
 		}
-		
+
 	}
 }
 
@@ -107,12 +102,63 @@ void parseOption(char* option) {
 
 void readDirectory (char* dir) {
 
-	stuct dirent entityQueue[10];
+	int entityQueueMax = 10;
+	struct dirent* entityQueue[entityQueueMax];
+	int entityQueueCount = 0;
 	struct dirent* pDirent;
 	DIR *pDir;
 
+	char* dirQueue[entityQueueMax];
+	int dirQueueCount = 0;
 
+	// Check if dir itself is an accessable directory
+	pDir = opendir(dir);
+	if (pDir == NULL) {
+		printf("%s", dir);
+		return;
+	}
 
+	// Read all the entities in dir
+	while ((pDirent = readdir(pDir)) != NULL) {
+
+		// Check if entityQueue is full
+		if (entityQueueCount == entityQueueMax) {
+			//TODO: remalloc bigger array
+		}
+
+		if (pDirent->d_name[0] != '.') {
+			entityQueue[entityQueueCount] = pDirent;
+			entityQueueCount++;
+		}
+	}
+
+	//sortEntityQueue(entityQueue);
+
+	for (int i = 0; i < entityQueueCount; i++) {
+		// print the entity
+		printf("%s  ", entityQueue[i]->d_name);
+		// Create a directory string for it
+		char* goDir = malloc(255);
+		strcpy(goDir, dir);
+		strcat(goDir, "/");
+		strcat(goDir, entityQueue[i]->d_name);
+
+		// Check if the entity was an accessible directory
+		pDir = opendir(goDir);
+
+		if (pDir != NULL) {
+			// This is an accessable directory, enqueue it
+			dirQueue[dirQueueCount] = goDir;
+			dirQueueCount++;
+		}
+	}
+	for (int i = 0; i < dirQueueCount; i++) {
+		readDirectory(dirQueue[i]);
+	}
+
+	//free(entityQueue);
+	//free(dirQueue);
+	return;
 }
 
 void sortEntityQueue(struct dirent* entityQueue) {
